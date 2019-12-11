934887edc5428fc2d6ed8a6b2e9b50f8bce195d3
Rico Chao <rchao@sfu.ca>
2019-12-10 20:18:34 -0800

    First commit of the project

---
+++ DebtTracker/DebtTracker/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  DebtTracker
+//
+//  Created by Rico Chao on 2019-12-10.
+//  Copyright © 2019 Rico Chao. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginTapped(_ sender: Any) {
+    }
+    
+    
+}
+
